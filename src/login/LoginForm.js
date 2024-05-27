c469ab57652c604b7ec7cb918199a7166fc5e37f
tensxi <bakooofficial@gmail.com>
2024-05-27 22:51:01 +0500

    Added basic validation and more forms of user information

--- src/login/LoginForm.js
+++ src/login/LoginForm.js
@@ -1,34 +1,84 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
 import axios from 'axios';
 
+
 const LoginForm = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const {register, handleSubmit, formState: {errors}, reset} = useForm();
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const [UserNameAdd, setUserNameAdd] = useState(false);
+    const [userEmail, setUserEmail] = useState('');
+
 
     const onSubmit = async (data) => {
         try {
             const response = await axios.post('http://localhost:8000/api/login/', data);
             console.log('Success:', response.data);
+            setUserEmail(data.email)
+            setSuccessMessage("Успешная авторизация");
+            setUserNameAdd(true);
+            reset()
         } catch (error) {
             console.error('Error:', error);
         }
     };
 
-    return (
+
+
+return (
+    <>
+        <div>
+            {UserNameAdd ? (
+                <div align="right">
+                    <h2>Вы успешно вошли в систему</h2>
+                    <h1>{userEmail}</h1>
+                </div>
+            ) : (
+                <div>
+                    <h1 align="right">Пользователь не вошел в систему</h1>
+                </div>
+            )}
+        </div>
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label>Email:</label>
-                <input name="email" {...register('email', { required: true })} />
+                <input name="email"
+                       type="email"
+                       {...register('email', {
+                           required: 'Email is required',
+                           pattern: {
+                               value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                               message: 'Invalid email address'
+                           }
+                       })}
+                />
                 {errors.email && <span>Email is required</span>}
             </div>
             <div>
                 <label>Password:</label>
-                <input name="password" type="password" {...register('password', { required: true })} />
+                <input
+                    name="password"
+                    type="password"
+                    {...register('password', {
+                        required: 'Password is required',
+                        minLength: {
+                            value: 8,
+                            message: 'Password must be at least 8 characters long'
+                        }
+                    })}
+                />
                 {errors.password && <span>Password is required</span>}
             </div>
             <button type="submit">Login</button>
+            {successMessage && <div>{successMessage}</div>}
         </form>
-    );
-};
+
+    </>
+
+
+);
+    }
+;
 
 export default LoginForm;
