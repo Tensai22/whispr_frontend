904129173047bc787343bf1983f09319899b66bc
KhanekShakh <KhanekShakh@gmail.com>
2024-05-30 12:26:29 +0500

    Changed Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import 'C:\Users\KhanekShakh\Desktop\whispr_frontend\src\HTML_CSS_JavaScript\login_registration\log_reg.css';
+import InstaLogo from 'C:\Users\KhanekShakh\Desktop\whispr_frontend\src\HTML_CSS_JavaScript\login_registration\Instagram_logo.png';
+import FaceBookLogo from 'C:\Users\KhanekShakh\Desktop\whispr_frontend\src\HTML_CSS_JavaScript\login_registration\FaceBook_logo.png';
+import TelegramLogo from 'C:\Users\KhanekShakh\Desktop\whispr_frontend\src\HTML_CSS_JavaScript\login_registration\Telegram_logo.png';
 
 const Footer = () => {
   return (
@@ -20,10 +23,10 @@ const Footer = () => {
       </div>
       <div className="footer-section">
         <h3>Соц.сети</h3>
-        <p>khngldi <a href="#"><img src="Instagram_logo.png" alt="Instagram" /></a> <a href="#"><img src="FaceBook_logo.png" alt="Facebook" /></a> <a href="#"><img src="Telegram_logo.png" alt="Telegram" /></a></p>
-        <p>p_a_r_r_o_t <a href="#"><img src="Instagram_logo.png" alt="Instagram" /></a> <a href="#"><img src="FaceBook_logo.png" alt="Facebook" /></a> <a href="#"><img src="Telegram_logo.png" alt="Telegram" /></a></p>
-        <p>tensxi <a href="#"><img src="Instagram_logo.png" alt="Instagram" /></a> <a href="#"><img src="FaceBook_logo.png" alt="Facebook" /></a> <a href="#"><img src="Telegram_logo.png" alt="Telegram" /></a></p>
-        <p>Bauka <a href="#"><img src="Instagram_logo.png" alt="Instagram" /></a> <a href="#"><img src="FaceBook_logo.png" alt="Facebook" /></a> <a href="#"><img src="Telegram_logo.png" alt="Telegram" /></a></p>
+        <p>khngldi <a href="#"><img src={InstaLogo} alt="Instagram" /></a> <a href="#"><img src={FaceBookLogo} alt="Facebook" /></a> <a href="#"><img src={TelegramLogo} alt="Telegram" /></a></p>
+        <p>p_a_r_r_o_t <a href="#"><img src={InstaLogo} alt="Instagram" /></a> <a href="#"><img src={FaceBookLogo} alt="Facebook" /></a> <a href="#"><img src={TelegramLogo} alt="Telegram" /></a></p>
+        <p>tensxi <a href="#"><img src={InstaLogo} alt="Instagram" /></a> <a href="#"><img src={FaceBookLogo} alt="Facebook" /></a> <a href="#"><img src={TelegramLogo} alt="Telegram" /></a></p>
+        <p>Bauka <a href="#"><img src={InstaLogo} alt="Instagram" /></a> <a href="#"><img src={FaceBookLogo} alt="Facebook" /></a> <a href="#"><img src={TelegramLogo} alt="Telegram" /></a></p>
       </div>
     </footer>
   );
