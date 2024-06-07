ec23bb09683be938782512b815979a6cab2a1f81
tensxi <bakooofficial@gmail.com>
2024-06-07 12:59:43 +0500

    -Наконец-то добавили нормальный чат и сохранение токенов в сеансе

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import * as ReactRouterDOM from "react-router-dom";
-import Chat from "./chatting/Chat";
 import Login from './authorization/Login';
 import Register from "./authorization/Registration";
 import ForgotPasswordPage from "./authorization/ResetPassword";
@@ -26,10 +25,9 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/" element={main_page()}/>
-                <Route path="/chat" element={<Chat/>}/>
+                <Route path="/chat" element={<ChatDesign/>}/>
                 <Route path="/registration" element={<Register/>} />
                 <Route path="/reset_password" element={<ForgotPasswordPage/>}/>
-                <Route path="/chatdesign" element={<ChatDesign/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/changepassword" element={<ChangePassword/>}/>
                 <Route path="/welcomepage" element={<WelcomePage/>}/>
--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,17 +1,64 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import '../css/chat.css';
-import { Form, Button, InputGroup, FormControl } from 'react-bootstrap';
+import {Form, Button, InputGroup, FormControl} from 'react-bootstrap';
+import axios from "axios";
 
 const ChatWindow = () => {
+
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState('');
+
+    useEffect(() => {
+        fetchMessages();
+        const interval = setInterval(fetchMessages, 5000);
+        return () => clearInterval(interval);
+    }, []);
+
+    const fetchMessages = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/get_messages/', {
+                withCredentials: true  // Включение сессионных куки
+            });
+            setMessages(response.data.messages);
+        } catch (error) {
+            console.error('Error fetching messages:', error);
+        }
+    };
+
+    const handleSendMessage = async () => {
+        try {
+            const response = await axios.post('http://localhost:8000/api/send_message/', {
+                text: newMessage
+            }, {
+                withCredentials: true  // Включение сессионных куки
+            });
+            setMessages([response.data, ...messages]);
+            setNewMessage('');
+        } catch (error) {
+            console.error('Error sending message:', error);
+        }
+    };
+
+
     return (
         <div className="chat-window">
             <div className="messages">
+                {messages.map((msg) => (
+                    <div key={msg.id} className="message">
+                        <img src={require("../assets/default_profile_image.jpeg")} alt="pfp" className="avatar"/>
+                        <strong className="sender">{msg.sender}:{msg.text}</strong>
+                            <em className="send_time">{new Date(msg.timestamp).toLocaleTimeString()}</em>
 
+                    </div>
+                ))}
             </div>
             <Form className="message-input">
                 <InputGroup>
-                    <FormControl placeholder="Написать сообщение..." />
-                    <Button className="btn-custom" type="submit" variant="primary">Отправить</Button>
+                    <FormControl type="text"
+                    value={newMessage}
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    placeholder="Напишите сообщение..."/>
+                    <Button className="btn-custom" type="submit" variant="primary" onClick={handleSendMessage}>Отправить</Button>
                 </InputGroup>
             </Form>
         </div>
