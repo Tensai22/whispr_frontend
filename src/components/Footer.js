fb876098e256ec7c4d261767bcb1e9bfc2357d80
Khangeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2024-11-25 22:20:18 +0500

    Изменил первую страницу и футер!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,34 +2,21 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/footer.css';
 
-
 const Footer = () => {
-
   return (
     <footer>
-      <div className="footer-section">
-        <h3>Ном.телефона</h3>
-        <p>+77007826147</p>
-        <p>+77073940282</p>
-        <p>+77086904270</p>
-        <p>+77081773609</p>
-        <p>+77078803476</p>
+      <div className="footer-content">
+        <div className="footer-section">
+          <h3>Соц. сети:</h3>
+          <p>khngldi <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /> <img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
+          <p>p_a_r_r_o_t <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
+          <p>tensxi <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
+          <p>Bauka <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
+          <p>senjumarru <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
+        </div>
       </div>
-      <div className="footer-section">
-        <h3>Эл.почта</h3>
-        <p>khanekshakh@gmail.com</p>
-        <p>nurik.shakh@gmail.com</p>
-        <p>tensxi@gmail.com</p>
-        <p>B.Bissanov@gmail.com</p>
-        <p>alinursarsenov315@gmail.com</p>
-      </div>
-      <div className="footer-section">
-        <h3>Соц.сети</h3>
-        <p>khngldi <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /> <img src={require("../assets/FaceBook_logo.png")} alt="Facebook" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-        <p>p_a_r_r_o_t <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/FaceBook_logo.png")} alt="Facebook" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-        <p>tensxi <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/FaceBook_logo.png")} alt="Facebook" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-        <p>Bauka <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/FaceBook_logo.png")} alt="Facebook" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-        <p>senjumarru <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/FaceBook_logo.png")} alt="Facebook" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
+      <div className="footer-bottom">
+        Ⓒ 2024 Whispr. Все права защищены.
       </div>
     </footer>
   );
