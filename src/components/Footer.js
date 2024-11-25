511d042dbd64d48e48039cc2a23feb20f4749ccc
Khangeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2024-11-25 22:50:23 +0500

    Добавил ссылки на соц сети в футере!

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,16 +5,53 @@ import '../css/footer.css';
 const Footer = () => {
   return (
     <footer>
-      <div className="footer-content">
-        <div className="footer-section">
-          <h3>Соц. сети:</h3>
-          <p>khngldi <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /> <img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-          <p>p_a_r_r_o_t <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-          <p>tensxi <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-          <p>Bauka <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-          <p>senjumarru <img src={require("../assets/Instagram_logo.png")} alt="Instagram" /><img src={require("../assets/Telegram_logo.png")} alt="Telegram" /></p>
-        </div>
-      </div>
+<div className="footer-content">
+  <div className="footer-section">
+    <h3>Соц. сети:</h3>
+    <p>
+      khngldi
+      <a href="https://www.instagram.com/khngldi/" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Instagram_logo.png")} alt="Instagram" />
+      </a>
+      <a href="https://t.me/khngldi" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Telegram_logo.png")} alt="Telegram" />
+      </a>
+    </p>
+    <p>
+      p_a_r_r_o_t
+      <a href="https://www.instagram.com/p___a___r___r___o___t/" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Instagram_logo.png")} alt="Instagram" />
+      </a>
+    </p>
+    <p>
+      tensxi
+      <a href="https://www.instagram.com/yrtensai/" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Instagram_logo.png")} alt="Instagram" />
+      </a>
+      <a href="https://t.me/IJWtDSiDH" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Telegram_logo.png")} alt="Telegram" />
+      </a>
+    </p>
+    <p>
+      Bauka
+      <a href="https://www.instagram.com/b.b.baurhzan/" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Instagram_logo.png")} alt="Instagram" />
+      </a>
+      <a href="https://t.me/Lazyasshole" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Telegram_logo.png")} alt="Telegram" />
+      </a>
+    </p>
+    <p>
+      senjumarru
+      <a href="https://www.instagram.com/senjumarru/" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Instagram_logo.png")} alt="Instagram" />
+      </a>
+      <a href="https://t.me/aliexxprs" target="_blank" rel="noopener noreferrer">
+        <img src={require("../assets/Telegram_logo.png")} alt="Telegram" />
+      </a>
+    </p>
+  </div>
+</div>
       <div className="footer-bottom">
         Ⓒ 2024 Whispr. Все права защищены.
       </div>
