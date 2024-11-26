d8fd2b34eeaae9cd5c8168ce1580a7f94ca081c9
Khangeldi Shakhmukhanbetov <khanekshakh@gmail.com>
2024-11-26 13:13:45 +0500

    Изменил дизайн при нажатии кнопки "Новая группа"!

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,44 @@
 import React, { useState } from 'react';
 import '../css/chat.css';
 import { ListGroup, InputGroup, FormControl, Nav, Button } from 'react-bootstrap';
+import Messenger from "../chatting/Messenger";
 
 const Sidebar = () => {
     const [activeTab, setActiveTab] = useState('chats');
     const [showMenu, setShowMenu] = useState(false);
+    const [isCreatingGroup, setIsCreatingGroup] = useState(false);
+    const [groupStep, setGroupStep] = useState(1);
+    const [selectedUsers, setSelectedUsers] = useState([]);
+    const [groupPhoto, setGroupPhoto] = useState(require('../assets/communityPic.png'));
+    const [groupName, setGroupName] = useState('');
+    const [groupDescription, setGroupDescription] = useState('');
+    const users = ['Пользователь 1', 'Пользователь 2', 'Пользователь 3'];
 
     const handleTabSelect = (tab) => setActiveTab(tab);
 
     const toggleMenu = () => {
         setShowMenu(!showMenu);
+        setIsCreatingGroup(false);
+        setGroupStep(1);
         if (!showMenu) {
-            document.body.classList.add('modal-open');  // Добавляем класс для размытия фона
+            document.body.classList.add('modal-open');
         } else {
-            document.body.classList.remove('modal-open');  // Убираем класс при закрытии меню
+            document.body.classList.remove('modal-open');
+        }
+    };
+
+    const handleAddToGroup = (user) => {
+        setSelectedUsers((prev) =>
+            prev.includes(user) ? prev.filter((u) => u !== user) : [...prev, user]
+        );
+    };
+
+    const handlePhotoChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = () => setGroupPhoto(reader.result);
+            reader.readAsDataURL(file);
         }
     };
 
@@ -27,7 +52,7 @@ const Sidebar = () => {
                             <img src={require("../assets/New_chat.png")} alt="Новый чат" />
                         </button>
                         <InputGroup className="mb-3">
-                            <FormControl placeholder="Поиск..." />
+                            <Messenger/>
                         </InputGroup>
                         <Nav variant="tabs" defaultActiveKey="chats" onSelect={handleTabSelect}>
                             <Nav.Item>
@@ -52,32 +77,142 @@ const Sidebar = () => {
             </div>
 
             {showMenu && (
-         <div className="menu-overlay">
-    <div className="menu-content">
-        <h2 className="menu-title">Новый Чат</h2>
-        <InputGroup className="mb-3 menu-search">
-            <FormControl placeholder="Поиск по имени..." />
-        </InputGroup>
-        <div className="menu-buttons">
-            <Button variant="outline-light" className="menu-button">
-                <img src={require("../assets/communityPic.png")} alt="Группа" className="menu-icon" />
-                Новая группа
-            </Button>
-            <Button variant="outline-light" className="menu-button">
-                <img src={require("../assets/community.png")} alt="Сообщество" className="menu-icon" />
-                Новое сообщество
-            </Button>
-        </div>
-        <h5>Пользователи</h5>
-        <ListGroup className="user-list">
-            {/* Здесь список пользователей рололо */}
-        </ListGroup>
-        <Button variant="danger" className="close-menu" onClick={toggleMenu}>Закрыть</Button>
-    </div>
+                <div className="menu-overlay">
+                    <div className="menu-content">
+                        {!isCreatingGroup && groupStep === 1 ? (
+                            <>
+                                <h2 className="menu-title">Новый Чат</h2>
+                                <InputGroup className="mb-3 menu-search">
+                                    <FormControl placeholder="Поиск по имени..." />
+                                </InputGroup>
+                                <div className="menu-buttons">
+                                    <Button
+                                        variant="outline-light"
+                                        className="menu-button"
+                                        onClick={() => setIsCreatingGroup(true)}
+                                    >
+                                        <img
+                                            src={require('../assets/communityPic.png')}
+                                            alt="Группа"
+                                            className="menu-icon"
+                                        />
+                                        Новая группа
+                                    </Button>
+                                    <Button variant="outline-light" className="menu-button">
+                                        <img
+                                            src={require('../assets/community.png')}
+                                            alt="Сообщество"
+                                            className="menu-icon"
+                                        />
+                                        Новое сообщество
+                                    </Button>
+                                </div>
+                                <h5>Пользователи</h5>
+                                <ListGroup className="user-list">
+                                    {/* Здесь список пользователей */}
+                                </ListGroup>
+                                <Button
+                                    variant="danger"
+                                    className="close-menu"
+                                    onClick={toggleMenu}
+                                >
+                                    Закрыть
+                                </Button>
+                            </>
+                        ) : groupStep === 1 && (
+                            <>
+                                <h2 className="menu-title">Добавление в группу</h2>
+                                <InputGroup className="mb-3 menu-search">
+                                    <FormControl placeholder="Поиск пользователей..." />
+                                </InputGroup>
+                                <ListGroup className="user-list">
+                                    {users.map((user, index) => (
+                                        <ListGroup.Item
+                                            key={index}
+                                            action
+                                            onClick={() => handleAddToGroup(user)}
+                                            active={selectedUsers.includes(user)}
+                                        >
+                                            {user}
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                                <div className="menu-buttons">
+                                    <Button
+                                        variant="secondary"
+                                        className="menu-button"
+                                        onClick={() => setIsCreatingGroup(false)}
+                                    >
+                                        Назад
+                                    </Button>
+                                    <Button
+                                        variant="primary"
+                                        className="menu-button"
+                                        disabled={selectedUsers.length === 0}
+                                        onClick={() => setGroupStep(2)}
+                                    >
+                                        Далее
+                                    </Button>
+                                </div>
+                            </>
+                        )}
 
-</div>
+                        {groupStep === 2 && (
+                            <div className="menu-content">
+                                <h2 className="menu-title">Создание группы</h2>
+                                <div className="group-photo-container">
 
+                                    <label htmlFor="group-photo-upload" className="group-photo-label">
+                                        <span className="photo-text">
+                                            <img className="menu-icon"
+                                            src={groupPhoto} /></span>
+                                    </label>
+                                    <input
+                                        id="group-photo-upload"
+                                        type="file"
+                                        accept="image/*"
+                                        onChange={handlePhotoChange}
+                                        style={{ display: 'none' }}
+                                    />
+                                </div>
 
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        placeholder="Название группы"
+                                        value={groupName}
+                                        onChange={(e) => setGroupName(e.target.value)}
+                                        style={{ backgroundColor: '#d3d3d3' }}
+                                    />
+                                </InputGroup>
+                                <InputGroup className="mb-3">
+                                    <FormControl
+                                        as="textarea"
+                                        placeholder="Описание группы"
+                                        value={groupDescription}
+                                        onChange={(e) => setGroupDescription(e.target.value)}
+                                    />
+                                </InputGroup>
+                                <div className="menu-buttons">
+                                    <Button
+                                        variant="secondary"
+                                        className="menu-button"
+                                        onClick={() => setGroupStep(1)}
+                                    >
+                                        Назад
+                                    </Button>
+                                    <Button
+                                        variant="primary"
+                                        className="menu-button"
+                                        disabled={!groupName.trim()}
+                                        onClick={() => alert('Группа успешно создана!')}
+                                    >
+                                        Далее
+                                    </Button>
+                               </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
             )}
         </>
     );
